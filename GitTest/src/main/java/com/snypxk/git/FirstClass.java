8713e85175c290ba56cc77d3eab6393bd549bcbd
snypxk <1452153881@qq.com>
2019-11-03 22:34:27 +0800

    初始化并第一次提交到本地仓库

---
+++ GitTest/src/main/java/com/snypxk/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.snypxk.git;
+
+/**
+ * @ClassName: FirstClass
+ * @Description: TODO
+ * @Author: snypxk
+ * @Date: 2019/11/3 22
+ * @Other:
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello, world!!");
+    }
+}

3159390abec5aa735d14c5a78c16e1ade9b79d49
snypxk <1452153881@qq.com>
2019-11-03 22:35:49 +0800

    修改FirstClass.java,然后第二次提交到本地仓库

--- GitTest/src/main/java/com/snypxk/git/FirstClass.java
+++ GitTest/src/main/java/com/snypxk/git/FirstClass.java
@@ -11,4 +11,8 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello, world!!");
     }
+
+    public void test(){
+        System.out.println("add content second times.....");
+    }
 }
